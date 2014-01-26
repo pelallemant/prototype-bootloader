8f06ee87a7c613f4ee90b40032e8781fccd99dec
Pierre Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2014-01-26 23:56:53 +0100

    bootloader.asm corrected, and one example added

--- src/x86/bootloader.asm
+++ src/x86/bootloader.asm
@@ -49,10 +49,57 @@ start:
     int 0x13          ; BIOS INTERUPT: int 0x13 with ah = 0x02: Read Sectors From Drive
     pop es            ; load es state
 
+; initialisation du pointeur sur la GDT
+    mov ax, gdtend    ; calcule la limite de GDT
+    mov bx, gdt
+    sub ax, bx
+    mov word [gdtptr], ax
+
+    xor eax, eax      ; calcule l'adresse lineaire de GDT
+    xor ebx, ebx
+    mov ax, ds
+    mov ecx, eax
+    shl ecx, 4
+    mov bx, gdt
+    add ecx, ebx
+    mov dword [gdtptr+2], ecx
+
+; passage en modep
+    cli
+    lgdt [gdtptr]    ; charge la gdt
+    mov eax, cr0
+    or  ax, 1
+    mov cr0, eax        ; PE mis a 1 (CR0)
+
+    jmp next
+next:
+    mov ax, 0x10        ; segment de donne
+    mov ds, ax
+    mov fs, ax
+    mov gs, ax
+    mov es, ax
+    mov ss, ax
+    mov esp, 0x9F000
+    
 ; jump to kernel
-    jmp dword BASE:0
+    jmp dword 0x8:0x1000
 
 ; ---------------------------------
 
 bootdrv:  db 0
+gdt:
+    db 0, 0, 0, 0, 0, 0, 0, 0
+gdt_cs:
+    db 0xFF, 0xFF, 0x0, 0x0, 0x0, 10011011b, 11011111b, 0x0
+gdt_ds:
+    db 0xFF, 0xFF, 0x0, 0x0, 0x0, 10010011b, 11011111b, 0x0
+gdtend:
+
+gdtptr:
+    dw 0  ; limite
+    dd 0  ; base
+
+;; NOP jusqu'a 510
+times 510-($-$$) db 144
+dw 0xAA55
 
